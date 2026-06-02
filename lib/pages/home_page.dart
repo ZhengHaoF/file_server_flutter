92966620b3c2d09004cd5b63e64c41f5abedb689
zhf8 <zhf8@foxhis.com>
2026-06-02 09:06:09 +0800

    refactor: 移除EdgeSwipeBack侧边返回组件
    删除了不再使用的edge_swipe_back组件，并从设置页和首页移除对该组件的引用

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,7 +11,6 @@ import '../services/api_service.dart';
 import '../services/storage_service.dart';
 import '../utils/date_utils.dart';
 import '../utils/file_type_utils.dart';
-import '../widgets/edge_swipe_back.dart';
 import '../widgets/file_icon.dart';
 import '../widgets/network_log_dialog.dart';
 import 'settings_page.dart';
@@ -523,10 +522,7 @@ class _HomePageState extends State<HomePage> {
         if (didPop) return;
         _handleWillPop(context);
       },
-      child: EdgeSwipeBack(
-        enabled: !_isRoot,
-        onPop: _goBack,
-        child: Scaffold(
+      child: Scaffold(
       appBar: AppBar(
         leading: _isRoot
             ? null
@@ -584,7 +580,6 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
         ],
-      ),
       ),
       ),
     );
